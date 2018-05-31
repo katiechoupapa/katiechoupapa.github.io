19ee590a587b9f1922f8599b0f4973da67cd3e13
katiechou <katiechoustudio@gmail.com>
2018-06-01 01:50:15 +1000

    modify service link

--- src/blocks/Service/index.js
+++ src/blocks/Service/index.js
@@ -10,19 +10,19 @@ class Service extends React.PureComponent {
             <div className="col-md-4">
               <div className="partnerships sub-title">
                 Partnerships
-                <a href="#" target="_blank" className="more">more</a>
+                <a href="http://www.google.com" target="_blank" className="more">more</a>
               </div>
             </div>
             <div className="col-md-4">
               <div className="immigration-assistance sub-title">
                 Immigration Assistance
-                <a href="#" target="_blank" className="more">more</a>
+                <a href="http://fb.com" target="_blank" className="more">more</a>
               </div>
             </div>
             <div className="col-md-4">
               <div className="property-development sub-title">
                 Property Development
-                <a href="#" target="_blank" className="more">more</a>
+                <a href="http://tw.yahoo.com" target="_blank" className="more">more</a>
               </div>
             </div>
           </div>
