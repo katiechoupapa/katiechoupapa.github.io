a3365026de40291d50cd7c87c4ab5875898c9100
sexyoung <Habuche@gmail.com>
2018-06-06 10:37:20 +0800

    區塊間視差背景 #14

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Parallax, Background } from 'react-parallax';
 
 import {
   Menu,
@@ -13,15 +14,24 @@ import {
   Contact,
 } from './blocks';
 
+import BG1 from './images/BG_01.png';
+import BG2 from './images/BG_02.png';
+import BG3 from './images/BG_03.png';
+import BG4 from './images/BG_04.png';
+
 class HomePage extends React.Component {
   render () {
     return (
-      <div>
+      <div className="home-page">
         <Home />
+        <Parallax bgImage={BG1} />
         <About />
+        <Parallax bgImage={BG2} />
         <Service />
+        <Parallax bgImage={BG3} />
         <Project />
         <Proposed />
+        <Parallax bgImage={BG4} />
         <Director />
         <Guarantee />
         <Contact />
